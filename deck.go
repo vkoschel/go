c7d40d67044b8549e9145577e352e39babf71883
vkoschel <victorkoschel@gmail.com>
2022-05-18 13:42:40 -0300

    shuffle deck

--- deck.go
+++ deck.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 type deck []string
@@ -49,4 +51,15 @@ func newDeckFromFile(filename string) deck {
 
 	s := strings.Split(string(bs), ",")
 	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPos := r.Intn(len(d) - 1)
+		d[i], d[newPos] = d[newPos], d[i]
+
+	}
 }
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 func main() {
-	cards := newDeckFromFile("vicor")
+	cards := newDeckFromFile("victor")
+	cards.print()
+	cards.shuffle()
 	cards.print()
 }
